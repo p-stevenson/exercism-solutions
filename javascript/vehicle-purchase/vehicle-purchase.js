5cd82b513625a8b76c74594416e1a96b3b65378d
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-02-15 10:46:39 +0000

    Solution for vehicle-purchase

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,7 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+  return (kind === "car" || kind === "truck") ? true:false
 }
 
 /**
@@ -23,7 +23,8 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+  const rest = ' is clearly the better choice.'
+  return option1 < option2 ? `${option1 + rest}` : `${option2 + rest}`
 }
 
 /**
@@ -35,5 +36,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if (age > 10) {
+    return originalPrice * 0.5
+  } else if (age <= 10 && age >=3) {
+    return originalPrice * 0.7 
+  } else {
+    return originalPrice * 0.8
+  }
 }
